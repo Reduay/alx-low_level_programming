8ed14af8c851c4f63d2a0827b88b6343a3fe6550
Reduay <redietnegash9@gmail.com>
2022-12-08 12:43:12 -0800

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Prints a string
+ *
+ *   *
+ *
+ *    * Return: 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char myStr[] = "\"Programming is like building a multilingual puzzle";
+
+
+
+			puts(myStr);
+
+
+
+				return (0);
+
+}
