16b8bcf79770f59c2dffd6108a3da48b4774e530
Reduay <105547293+Reduay@users.noreply.github.com>
2022-12-08 13:26:52 -0800

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 
 /**
+ * main - Program starts here
  *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
-
+ * Return: 0, another return value is unnecessary
+ */
 int main(void)
-
 {
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 
-		printf("Size of a char: %i byte(s)\n", sizeof(char));
-
-			printf("Size of an int: %i byte(s)\n", sizeof(int));
-
-				printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-
-					printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-
-						printf("Size of a float: %i byte(s)\n", sizeof(float));
-
-							return (0);
-
+	return (0);
 }
