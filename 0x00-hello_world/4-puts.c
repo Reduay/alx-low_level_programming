4560e3848f5f2525610aedb6b6d7286fa2d061f6
Reduay <105547293+Reduay@users.noreply.github.com>
2022-12-08 14:05:39 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,29 +1,15 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Prints a string
  *
- *  * main - Prints a string
- *
- *   *
- *
- *    * Return: 0
- *
- *     */
-
+ * Return: 0
+ */
 int main(void)
-
 {
+	char myStr[] = "\"Programming is like building a multilingual puzzle";
 
-		char myStr[] = "\"Programming is like building a multilingual puzzle";
+	puts(myStr);
 
-
-
-			puts(myStr);
-
-
-
-				return (0);
-
+	return (0);
 }
