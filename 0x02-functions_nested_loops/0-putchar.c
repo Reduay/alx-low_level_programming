cece2489043a5412b6e10e14c35c0955584508af
Reduay <105547293+Reduay@users.noreply.github.com>
2022-12-13 13:13:28 -0800

    if

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,25 @@
-hello
+#include "main.h"
+
+/**
+ * main - Prints Holberton using _putchar()
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	/*
+	 * int Holberton[] = {72, 111, 108, 98, 101, 114, 116,
+			111, 110};
+	*/
+	char Holberton[] = "_putchar\n";
+	int ij;
+
+	for (ij = 0; ij < 9; ij++)
+	{
+		_putchar(Holberton[ij]);
+	}
+	/*
+	 * _putchar(10);
+	 */
+	return (0);
+}
